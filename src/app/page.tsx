6253987d8616a118863cf991d188a8ed53b4e8bc
mashamipacifique <mshamipaccy04@gmail.com>
2025-11-10 09:18:26 +0200

    initialized the project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button text="Hello" variant={"outline"} className="bg-red-500" />
+    </div>
+  );
+}
